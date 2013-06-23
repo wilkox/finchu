68b4a967e2f11c15247ab5ebf6a95dec4fb4b08a
David Wilkins <david@wilkox.org>
2013-06-23 16:31:12 +1000

    Initialised Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Finchu::Application.config.secret_token = '21dc86fc7c1352c297ceeaeb011bf0e5ef76bc799e1264a142e4f8aafa8876a2787f78a030a39a049385a8ef32e3b8593d6eb1091698dd0d104661f3903e7b76'
